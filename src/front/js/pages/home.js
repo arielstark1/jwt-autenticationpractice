6fa6270c66c55ec0a3df5ddce7991f8a5ca125b9
arielstark1 <arielmarvin@gmail.com>
2022-03-05 16:09:40 +0000

    cambio

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import ironmanUrl from "../../img/ironman.jpg";
 import "../../styles/home.css";
 
